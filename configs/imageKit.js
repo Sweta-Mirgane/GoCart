2e45ae48704438326ac9cd3bda92ab025c9f9da7
Sweta <swetamirgane@gmail.com>
2025-09-21 16:09:04 +0530

    fixed typo error

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -1,12 +1,10 @@
+// /configs/imageKit.js
 import ImageKit from "imagekit";
 
-
-var ImageKit = require("imagekit");
-
-var imagekit = new ImageKit({
-    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 export default imagekit;
